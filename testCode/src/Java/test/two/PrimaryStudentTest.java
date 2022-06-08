20e1ceed53b993f9fa3c9c02a9f13bb38f12b312
Lxzz24 <lxzz108@gmail.com>
2022-06-08 17:19:54 +0800

    OOP

---
+++ testCode/src/Java/test/two/PrimaryStudentTest.java
@@ -0,0 +1,48 @@
+package Java.test.two;
+
+public class PrimaryStudentTest {
+    public static void main(String[] args) {
+        PersonD p = new PersonD("小明", 12);
+        StudentD s = new StudentD("小红", 20, 99);
+        // TODO: 定义 PrimaryStudent，从 Student 继承，新增 grade 字段:
+        StudentD ps = new PrimaryStudent("小军", 9, 100, 5);
+        System.out.println(ps.getScore());
+    }
+}
+
+class PersonD {
+    protected String name;
+    protected int age;
+
+    public PersonD(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return name;}
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age;}
+    public void setAge(int age) { this.age = age; }
+}
+
+class StudentD extends PersonD {
+    protected int score;
+
+    public StudentD(String name, int age, int score) {
+        super(name, age);
+        this.score = score;
+    }
+
+    public int getScore() { return score;}
+}
+
+class PrimaryStudent extends StudentD {
+    // TODO
+    protected int grade;
+
+    public PrimaryStudent(String name, int age, int score, int grade) {
+        super(name, age, score);
+        this.grade = grade;
+    }
+}
